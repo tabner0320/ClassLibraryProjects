a3197015c2ccc18d99075a8f115b2205f334fa68
Theophilus (Theo) Abner <theophilusabner0204@yahoo.com>
2026-06-10 22:37:06 -0400

    Complete ClassLibraryProjects tutorial

---
+++ ShowCase/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UtilityLibraries;
+
+int row = 0;
+
+do
+{
+    if (row == 0 || row >= 25)
+        ResetConsole();
+
+    string? input = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(input))
+        break;
+
+    Console.WriteLine(
+        $"Input: {input} {"Begins with uppercase? ",30}: " +
+        $"{(input.StartsWithUpper() ? "Yes" : "No")}{Environment.NewLine}");
+
+    row += 3;
+
+} while (true);
+
+return;
+
+void ResetConsole()
+{
+    if (row > 0)
+    {
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+    Console.Clear();
+
+    Console.WriteLine(
+        $"{Environment.NewLine}Press <Enter> only to exit; otherwise, enter a string and press <Enter>:{Environment.NewLine}");
+
+    row = 3;
+}// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ StringLibraryTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace StringLibraryTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
